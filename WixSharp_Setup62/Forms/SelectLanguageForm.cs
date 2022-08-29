0b5c9c7a37ee26d9b9cd8e3a7f7faf34c597daf7
Arthur Neto <arthur.santos@printwayy.com>
2022-08-29 19:29:40 -0300

    Add project files.

---
+++ WixSharp_Setup62/Forms/SelectLanguageForm.cs
@@ -0,0 +1,88 @@
+﻿using System.Globalization;
+using System.Windows.Forms;
+
+namespace WixSharp_Setup62.Forms
+{
+    public class SelectLanguageForm : Form
+    {
+        public CultureInfo SelectedCulture { get; set; }
+
+        private ComboBox cbLanguages;
+        private Label lblSelectLanguage;
+        private Button btnOK;
+
+        public SelectLanguageForm()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            cbLanguages = new ComboBox();
+            btnOK = new Button();
+            lblSelectLanguage = new Label();
+            SuspendLayout();
+            // 
+            // cbLanguages
+            // 
+            cbLanguages.FormattingEnabled = true;
+            cbLanguages.Location = new System.Drawing.Point(86, 51);
+            cbLanguages.Name = "cbLanguages";
+            cbLanguages.Size = new System.Drawing.Size(197, 21);
+            cbLanguages.TabIndex = 0;
+            // 
+            // btnOK
+            // 
+            btnOK.Location = new System.Drawing.Point(209, 78);
+            btnOK.Name = "btnOK";
+            btnOK.Size = new System.Drawing.Size(75, 23);
+            btnOK.TabIndex = 2;
+            btnOK.Text = "OK";
+            btnOK.UseVisualStyleBackColor = true;
+            btnOK.Click += new System.EventHandler(btnOK_Click);
+            // 
+            // lblSelectLanguage
+            // 
+            lblSelectLanguage.AutoSize = true;
+            lblSelectLanguage.Location = new System.Drawing.Point(78, 25);
+            lblSelectLanguage.Name = "lblSelectLanguage";
+            lblSelectLanguage.Size = new System.Drawing.Size(205, 13);
+            lblSelectLanguage.TabIndex = 3;
+            lblSelectLanguage.Text = "Select Language:";
+            // 
+            // SelectLanguageForm
+            // 
+            AcceptButton = btnOK;
+            ClientSize = new System.Drawing.Size(296, 112);
+            Controls.Add(lblSelectLanguage);
+            Controls.Add(btnOK);
+            Controls.Add(cbLanguages);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Name = "SelectLanguageForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Select Language";
+            Load += new System.EventHandler(SelectLanguageForm_Load);
+            ResumeLayout(false);
+            PerformLayout();
+
+        }
+
+        private void btnOK_Click(object sender, System.EventArgs e)
+        {
+            SelectedCulture = (CultureInfo)cbLanguages.SelectedItem;
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private void SelectLanguageForm_Load(object sender, System.EventArgs e)
+        {
+            cbLanguages.Items.Add(Program.DefaultLanguage);
+            foreach (var supportedLanguage in Program.SupportedLanguages)
+            {
+                cbLanguages.Items.Add(supportedLanguage);
+            }
+            cbLanguages.DisplayMember = nameof(CultureInfo.DisplayName);
+            cbLanguages.SelectedIndex = 0;
+        }
+    }
+}
---
+++ WixSharp_Setup62/Program.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.Deployment.WindowsInstaller;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Security.Principal;
+using System.Text;
+using WixSharp;
+using WixSharp.CommonTasks;
+using WixSharp_Setup62.Forms;
+
+namespace WixSharp_Setup62
+{
+    internal class Program
+    {
+        public static readonly CultureInfo DefaultLanguage = new CultureInfo("en-US");
+        public static readonly IList<CultureInfo> SupportedLanguages = new List<CultureInfo>
+        {
+            new CultureInfo("pt-BR"),
+        };
+
+        private static void Main()
+        {
+            var project = new ManagedProject(
+                "XYZ",
+                new Dir(@"%ProgramFiles%\Teste\",
+                    new File("Program.cs")
+                )
+            )
+            {
+                GUID = new Guid("6fe30b47-2577-43ad-9095-1861ba25889b"),
+                LocalizationFile = $@"Languages\{DefaultLanguage.Name}.wxl",
+
+                BannerImage = @"Resources\dialog.png",
+                BackgroundImage = @"Resources\dialog.png",
+                LicenceFile = @"Resources\license.rtf",
+
+                ManagedUI = new ManagedUI(),
+            };
+
+            project.UIInitialized += (SetupEventArgs e) =>
+            {
+                CheckElevatedRights(e);
+            };
+
+            project.ManagedUI.InstallDialogs.Add<WelcomeDialog>()
+                                            .Add<InstallDirDialog>()
+                                            .Add<ProgressDialog>()
+                                            .Add<ExitDialog>();
+
+            project.ManagedUI.ModifyDialogs.Add<MaintenanceTypeDialog>()
+                                           .Add<ProgressDialog>()
+                                           .Add<ExitDialog>();
+
+            Localize(project);
+
+            project.Language = DefaultLanguage.Name;
+            var productMsi = project.BuildMsi();
+
+            var sb = new StringBuilder();
+            sb.Append(DefaultLanguage.Name);
+            foreach (var supportedLanguage in SupportedLanguages)
+            {
+                project.Language = supportedLanguage.Name;
+                var mstFile = project.BuildLanguageTransform(productMsi, supportedLanguage.Name, $@"Languages\{supportedLanguage.Name}.wxl");
+
+                productMsi.EmbedTransform(mstFile);
+
+                sb.Append($",{supportedLanguage.Name}");
+            }
+
+            productMsi.SetPackageLanguages(sb.ToString().ToLcidList());
+        }
+
+        private static void Localize(ManagedProject project)
+        {
+            project.AddBinary(new Binary(new Id($"{DefaultLanguage.Parent}_xsl"), $@"Languages\{DefaultLanguage.Name}.wxl"));
+
+            foreach (var supportedLanguage in SupportedLanguages)
+            {
+                project.AddBinary(new Binary(new Id($"{supportedLanguage.Parent}_xsl"), $@"Languages\{supportedLanguage.Name}.wxl"));
+            }
+
+            project.UIInitialized += (SetupEventArgs e) =>
+            {
+                var runtime = e.ManagedUI.Shell.MsiRuntime();
+
+                var languageSelected = SelectLanguage().Name;
+                if (languageSelected.Equals(DefaultLanguage.Name))
+                {
+                    runtime.UIText.InitFromWxl(e.Session.ReadBinary($"{DefaultLanguage.Parent}_xsl"));
+                    return;
+                }
+
+                foreach (var supportedLanguage in SupportedLanguages)
+                {
+                    if (supportedLanguage.Name.Equals(languageSelected))
+                    {
+                        runtime.UIText.InitFromWxl(e.Session.ReadBinary($"{supportedLanguage.Parent}_xsl"));
+                    }
+                }
+            };
+        }
+
+        private static CultureInfo SelectLanguage()
+        {
+            var selectLanguageForm = new SelectLanguageForm();
+
+            selectLanguageForm.ShowDialog();
+
+            return selectLanguageForm.SelectedCulture;
+        }
+
+        private static void CheckElevatedRights(SetupEventArgs setupEventArgs)
+        {
+            if (!new WindowsPrincipal(WindowsIdentity.GetCurrent()).IsInRole(WindowsBuiltInRole.Administrator))
+            {
+                setupEventArgs.Result = ActionResult.Failure;
+
+                var startInfo = new ProcessStartInfo
+                {
+                    UseShellExecute = true,
+                    WorkingDirectory = Environment.CurrentDirectory,
+                    FileName = "msiexec.exe",
+                    Arguments = "/i \"" + setupEventArgs.MsiFile + "\"",
+                    Verb = "runas"
+                };
+
+                Process.Start(startInfo);
+            }
+        }
+    }
+}
